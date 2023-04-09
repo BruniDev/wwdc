b80cbea79865f322963af28a3828f13ad3785040
Junghyeon Kim <hyeondxx@gmail.com>
2023-04-09 15:22:39 +0900

    Finished Adding Font

--- MyApp.swift
+++ MyApp.swift
@@ -2,6 +2,9 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+    init() {
+            FontManager.registerFonts()
+        }
     var body: some Scene {
         WindowGroup {
             OpenPageView()
