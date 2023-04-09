a906814e14f4cfe67f8fa824e62f5c80107dd279
Junghyeon Kim <hyeondxx@gmail.com>
2023-04-09 15:11:45 +0900

    Added ThreeDButton

---
+++ OpenPageView.swift
@@ -0,0 +1,20 @@
+//
+//  OpenPageView.swift
+//  wwdc
+//
+//  Created by 정현 on 2023/04/09.
+//
+
+import SwiftUI
+
+struct OpenPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OpenPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        OpenPageView()
+    }
+}
