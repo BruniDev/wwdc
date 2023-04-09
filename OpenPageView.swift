7ad7b36b41054291dcf37dd02369a6d58df3b55e
Junghyeon Kim <hyeondxx@gmail.com>
2023-04-09 15:43:20 +0900

    Added Color

--- OpenPageView.swift
+++ OpenPageView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct OpenPageView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .font(.custom("Digitalt",size:50))
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color("Purple_1"),Color("Purple_2"),Color("Purple_3"),]),
+                           startPoint: .top, endPoint: .bottom)
+            VStack(alignment: .center,spacing: 110) {
+                VStack(alignment: .center, spacing: -10){
+                    VStack(alignment: .center,spacing: -105){
+                        Text("Game")
+                            .font(.custom("Digitalt", size: 100))
+                            .foregroundColor(Color("Yellow_2"))
+                        Text("Game")
+                            .font(.custom("Digitalt", size: 100))
+                            .foregroundColor(Color("Yellow_1"))
+                    }
+                    VStack(alignment:.center,spacing: -145) {
+                        Text("Name")
+                            .font(.custom("Digitalt", size: 140))
+                            .foregroundColor(Color("Purple_0"))
+                        Text("Name")
+                            .font(.custom("Digitalt", size: 140))
+                            .foregroundColor(.white)
+                    }
+                }
+                Button {
+                    print("It's clicked")
+                    
+                } label :
+                {
+                    
+                    NavigationLink(destination: ContentView()) {
+                        Text("PLAY")
+                    }
+                }
+                .buttonStyle(ThreeDButton())
+                .font(.custom("Digitalt",size: 30))
+                .frame(width: 100, height: 30)
+                .foregroundColor(.white)
+                
+            }
+        }.ignoresSafeArea(.all)
     }
 }
 
